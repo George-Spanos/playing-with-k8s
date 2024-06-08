9ce1719a48b6f5c55a16bc963e0b14d6fcf24b20
George Spanos <gspanos@moby-it.com>
2024-06-09 01:39:35 +0300

    super glad it works

--- notes.md
+++ notes.md
@@ -6,10 +6,9 @@
   - install metallb in order to have a cluster loadbalancer
   - REMEMBER TO ADD PROXY PROTOCOL TO NGINX CONTROLLER
 
-It seems that cert manager might not play well with nginx helm from nginx. Try [this helm](https://kubernetes.github.io/ingress-nginx/)
+Ingress Notes
 
-Made it work! Test the following:
-
-- Do the issuers need to be on the same namespace?
+- Do the issuers need to be on the same namespace? **Yes**
 - Does the ingress need? `acme.cert-manager.io/http01-edit-in-place: "true", nginx.ingress.kubernetes.io/force-ssl-redirect: "false"`
-- does the app service need to be cluster ip? Is it better to be load balancer?
+  - **Only `acme.cert-manager.io/http01-edit-in-place: "true"` is needed**
+- does the app service need to be cluster ip? Is it better to be load balancer? It can be load balancer as well
--- post-install.sh
+++ post-install.sh
@@ -3,23 +3,24 @@
 # this script is intented to run when the kubectl has access to the cluster
 # in order to run some post set up stuff
 
+# docker registry secret. Remember to create this for every namespace that neeeds it
 kubectl create secret generic regcred \
   --from-file=.dockerconfigjson=/home/gspanos/.docker/config.json \
   --type=kubernetes.io/dockerconfigjson
 
-# nginx ingress controller
-kubectl create ns ingress-nginx
-helm install my-release oci://ghcr.io/nginxinc/charts/nginx-ingress --version 1.2.2 --namespace ingress-nginx
 
 
 # metallb
 helm repo add metallb https://metallb.github.io/metallb
-kubectl create namespace metallb-system
 helm install metallb metallb/metallb --namespace metallb-system --create-namespace
 kubectl apply -f metallb.yml
 
-# cert manager
+# nginx ingress controller
+kubectl create ns ingress-nginx
+helm install nginx-controller --namespace ingress-nginx --create-namespace ./nginx-ingress
 
+# cert managerz
+helm repo add jetstack https://charts.jetstack.io --force-update
 helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
